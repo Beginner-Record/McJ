c45207ebaa0da674f306891b80b8a250911c9e27
peubel <sksqkrwns@naver.com>
2022-04-27 21:28:01 +0900

    REAL-1 User Entity 설계

--- src/main/java/com/record/mcj/entity/User.java
+++ src/main/java/com/record/mcj/entity/User.java
@@ -1,11 +1,15 @@
 package com.record.mcj.entity;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id @GeneratedValue
