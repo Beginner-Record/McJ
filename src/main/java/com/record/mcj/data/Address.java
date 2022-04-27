d7d7a85716c09b32f56a0e5a53938fef2988c2a8
peubel <sksqkrwns@naver.com>
2022-04-27 23:24:01 +0900

    MCJ-1 User Entity Test code create

--- src/main/java/com/record/mcj/data/Address.java
+++ src/main/java/com/record/mcj/data/Address.java
@@ -9,4 +9,9 @@ public class Address {
     String zipcode, city, street;
 
 
+    public Address(String zipcode, String city, String street) {
+        this.zipcode = zipcode;
+        this.city = city;
+        this.street = street;
+    }
 }
--- src/main/java/com/record/mcj/entity/User.java
+++ src/main/java/com/record/mcj/entity/User.java
@@ -1,15 +1,20 @@
 package com.record.mcj.entity;
 
+import com.google.common.base.Preconditions;
 import com.record.mcj.data.Address;
 import com.record.mcj.data.Role;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.google.common.base.Preconditions.*;
 
 @Entity
 @Getter
@@ -29,7 +34,9 @@ public class User {
     @Embedded
     private Address address;
 
-    private LocalDate birthday;
+    private LocalDateTime birthday;
+
+    private LocalDateTime joinDate;
 
     @Enumerated(EnumType.STRING)
     private Role role;
@@ -37,12 +44,18 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Board> boards;
 
-    public User(Long id, String name, String email, String password, Address address, LocalDate birthday) {
-        this.id = id;
+    public User(String name, String email, String password, Address address, LocalDateTime birthday, Role role) {
+        checkArgument(Strings.isNotBlank(name) , "사용자 이름은 필수입니다.");
+        checkArgument(Strings.isNotBlank(email) , "이메일은 필수입니다.");
+        checkArgument(Strings.isNotBlank(password) , "패스워드는 필수입니다.");
+
+
+        this.joinDate = LocalDateTime.now();
         this.name = name;
         this.email = email;
         this.password = password;
         this.address = address;
         this.birthday = birthday;
+        this.role = role;
     }
 }
