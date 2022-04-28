a712dc08ee25689ddf9a1e34723d63e941e6eeed
peubel <sksqkrwns@naver.com>
2022-04-28 22:42:05 +0900

    MCJ-1 엔티티 레포지토리 설계

---
+++ src/test/java/com/record/mcj/repository/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.record.mcj.repository;
+
+import com.record.mcj.data.Address;
+import com.record.mcj.data.Role;
+import com.record.mcj.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import javax.annotation.security.RunAs;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = NONE)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Test
+    @Transactional
+    void 유저_생성_테스트(){
+        final User save = userRepository.save(new User("name", "email", "password", new Address("A", "B", "C"), LocalDateTime.now(), Role.user));
+
+        final User user = userRepository.findById(save.getId()).orElseThrow();
+
+        assertThat(user.getId()).isNotNull();
+
+    }
+
+    @Test
+    @Transactional
+    void 유저_삭제_테스트(){
+        final User save = userRepository.save(new User("name", "email", "password", new Address("A", "B", "C"), LocalDateTime.now(), Role.user));
+
+        userRepository.delete(save);
+
+        Assertions.assertThrows(NoSuchElementException.class , () -> userRepository.findById(save.getId()).orElseThrow());
+
+    }
+
+
+}
